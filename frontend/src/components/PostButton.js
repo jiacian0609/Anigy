20425215e2a06b147c674f6858386f978602d70a
jiacian0609 <79597797+jiacian0609@users.noreply.github.com>
2022-12-03 18:33:31 +0800

    post button

---
+++ frontend/src/components/PostButton.js
@@ -0,0 +1,98 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+import styled from "styled-components";
+
+const Container = styled.div `
+    width: 230px;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    position: relative;
+`
+
+const Img = styled.img `
+    width: 160px;
+    height: 120px;
+
+    position: absolute;
+    z-index: 1;
+    
+    background-color: #619E5C;
+`
+
+const ContentBox = styled.div `
+    width: 100%;
+
+    margin-top: 60px;
+    padding-top: 75px;
+    padding-bottom: 20px;
+
+    background-color: #F5F5F5;
+    box-shadow: 0px 3px 3px grey;
+    border-radius: 10%;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 10px;
+`
+
+const Row = styled.div `
+    width: 100%;
+    box-sizing: border-box;
+    padding: 0 20px;
+    display: flex;
+    gap: 10px;
+`
+
+const Field = styled.div `
+    padding: 2px 5px;
+
+    background-color: #619E5C;
+    color: #FFFFFF;
+`
+
+const Text = styled.div `
+    padding: 2px 5px;
+`
+
+
+function PostButton({ post }) {
+    return (
+        <Link to={`/post/${post.id}`} style={{ textDecoration: 'none', color: 'inherit'}}>
+            <Container>
+                <Img alt={post.id} />
+                <ContentBox>
+                    <Row>
+                        <Field>動物</Field>
+                        <Text>{post.animal}</Text>
+                    </Row>
+                    <Row>
+                        <Field>品種</Field>
+                        <Text>{post.breed}</Text>
+                    </Row>
+                    <Row>
+                        <Field>性別</Field>
+                        <Text>{post.sex === 'M' ? '公' : '母'}</Text>
+                    </Row>
+                    <Row>
+                        <Field>年齡</Field>
+                        <Text>{post.age}</Text>
+                    </Row>
+                    <Row>
+                        <Field>結紮</Field>
+                        <Text>{post.neutered ? '是' : '否'}</Text>
+                    </Row>
+                    <Row>
+                        <Field>地區</Field>
+                        <Text>{post.location}</Text>
+                    </Row>
+                </ContentBox>
+            </Container>
+        </Link>
+    )
+}
+
+export default PostButton;
--- frontend/src/containers/Main.js
+++ frontend/src/containers/Main.js
@@ -1,8 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from 'styled-components';
+
+import PostButton from "../components/PostButton";
+
+const Container = styled.div `
+    width: 100%;
+    height: 100%;
+
+    padding: 30px;
+`
 
 function Main() {
+    const samplePost = {
+        id: 1,
+        animal: '貓',
+        breed: '混種貓',
+        sex: 'F',
+        age: '1歲',
+        neutered: true,
+        location: '台北動物之家'
+    }
+    const [posts, setPosts] = useState([samplePost]);
+
     return (
-        <h1>Main</h1>
+        <Container>
+            {posts.map(p => 
+                <PostButton 
+                    key={p.id}
+                    post={p}
+                />)}
+        </Container>
     )
 }
 

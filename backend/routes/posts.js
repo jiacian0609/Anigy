c12ab0b8bf62e953a3517690cb12f3208b40187b
liangyuchen <cy9815526@gmail.com>
2022-12-11 16:05:43 +0800

    feat: update get post api

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -3,13 +3,35 @@ import Post from "../models/Post.js";
 
 const router = Router();
 
+/* GET posts data */
 router.get('/', async function(req, res, next) {
+    const user_id = req.body.user_id;
+    const post_id = req.body.post_id;
+    const age = req.body.age;
+    const sex = req.body.sex;
+    const animal = req.body.animal;
+    const location = req.body.location;
+
     try {
-		const allPosts = await Post.find();
-		return res.status(200).send(allPosts);
+        let allPosts;
+    
+        if(user_id) // Case 1: by user_id
+            allPosts= await Post.find({ user_id });
+        else if(post_id) // Case 2: by post_id
+            allPosts= await Post.find({ _id: post_id });
+        else { // Case 3: by filter
+            allPosts= await Post.find();
+            allPosts = allPosts.filter((post) => age ? post.age === age : true)
+            allPosts = allPosts.filter((post) => sex ? post.sex === sex : true)
+            allPosts = allPosts.filter((post) => animal ? post.animal === animal : true)
+            allPosts = allPosts.filter((post) => location ? post.location === location : true)
+        } 
+
+		return res.status(200).json({ data: allPosts });
 	}
-	catch (e) {
-		return res.status(400).send({ message: "Database failed" });
+	catch (error) {
+        //console.log(error.message)
+		return res.status(400).json({ error: 'Get posts error' });
 	}
 });
 

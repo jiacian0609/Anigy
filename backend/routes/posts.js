c044a79be2498e290b2f1789e55cb2988c035c25
liangyuchen <cy9815526@gmail.com>
2022-12-22 21:54:01 +0800

    update post post api

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,5 +1,9 @@
 import { Router } from "express";
 import Post from "../models/Post.js";
+import Age from "../models/Age.js";
+import Animal from "../models/Animal.js";
+import Breed from "../models/Breed.js";
+import Location from "../models/Location.js";
 import { authentication } from "../utils/util.js";
 
 const router = Router();
@@ -50,12 +54,55 @@ router.post('/', authentication(), async function(req, res, next) {
     const { animal, color, age, sex, image, neutered, location, contact, status, other_info, origin_url } = req.body
 
     try {
-        const newPost = new Post({ user_id, animal, color, age, sex, image, neutered, location, contact, status, other_info, origin_url });
+        const newPost = new Post({ 
+            user_id, 
+            animal: animal ?? null, 
+            color: color ?? null,
+            age: age ?? null,
+            sex: sex ?? null,
+            image: image ?? null,
+            neutered: neutered ?? null,
+            location: location ?? null,
+            contact: contact ?? null,
+            status: status ?? null,
+            other_info: other_info ?? null,
+            origin_url: origin_url ?? null,
+        });
         const addPost = await newPost.save();
+
+        // Check whether a new age
+        const existAge = await Age.find({ age });
+        if(existAge.length === 0) {
+            const newAge = new Age({ age });
+            const addAge = await newAge.save();
+        }
+
+        // Check whether a new animal
+        const existAnimal = await Animal.find({ animal });
+        if(existAnimal.length === 0) {
+            const newAnimal = new Animal({ animal });
+            const addAnimal = await newAnimal.save();
+        }
+
+        // Check whether a new breed
+        /* const existBreed = await Breed.find({ breed });
+        if(existBreed.length === 0) {
+            const newBreed = new Breed({ breed });
+            const addBreed = await newBreed.save();
+        } */
+
+        // Check whether a new location
+        const existLocation = await Location.find({ location });
+        if(existLocation.length === 0) {
+            const newLocation = new Location({ location });
+            const addLocation = await newLocation.save();
+        }
+
+
 		return res.status(200).json({ data: addPost, message: 'Add Success' });
 	}
 	catch (error) {
-        //console.log(error.message)
+        console.log(error.message)
 		return res.status(400).json({ error: 'Add a post error' });
 	}
 });

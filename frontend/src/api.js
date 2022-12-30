f2067fa48810cf4552c1ae338ab54fe0b09eac70
jiacian0609 <79597797+jiacian0609@users.noreply.github.com>
2022-12-30 12:25:45 +0800

    feat: get user post api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,11 +1,23 @@
 import axios from 'axios';
 
 const hostname = 'http://localhost:4000/api';
+const jwt = localStorage.getItem('JWT');
 
 export const api = {
     getAllPost() {
         return (
             axios.get(`${hostname}/posts/all`)
+            .then(res => res.data)
+            .catch(err => console.log(err))
+        )
+    },
+    getUserPost() {
+        return (
+            axios.get(`${hostname}/posts`, {
+                headers: {
+                    authorization: `Bearer ${jwt}`
+                }
+            })
             .then(res => res.data)
             .catch(err => console.log(err))
         )
